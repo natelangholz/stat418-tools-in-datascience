b1ad2ecd25d3bd951e0784e4087ac93576704f78
tannerkoscinski <tannerkoscinski@gmail.com>
2019-04-23 20:55:24 +0000

    Homework 1

--- week-2/hw1/homework-submissions/hw1_tanner_koscinski.sh
+++ week-2/hw1/homework-submissions/hw1_tanner_koscinski.sh
@@ -10,43 +10,43 @@ echo
 echo '1. List the top 10 web sites from which requests came (non-404 status, external addresses looking in).'
 echo
 echo Count Address
-#curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 != 404) {print $11}}' | grep '^"-"$' -v | grep '^$' -v | sort | uniq -c | sort -nr | head -n 10
+curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 != 404) {print $11}}' | grep '^"-"$' -v | grep '^$' -v | sort | uniq -c | sort -nr | head -n 10
 echo
 
 # 2. List the top 10 local web pages requested (non-404 status).
 echo '2. List the top 10 local web pages requested (non-404 status).'
 echo
 echo Count Page
-#curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 != 404) {print $7}}' | sort | uniq -c | sort -nr | head -n 10
+curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 != 404) {print $7}}' | sort | uniq -c | sort -nr | head -n 10
 echo
 
 # 3. List the top 10 web browsers used to access the site. It is not necessary to get fancy and parse out all of the browser string. Simply print out the information that is there. Display the percentage of all browser types that each line represents.
 echo '3. List the top 10 web browsers used to access the site. It is not necessary to get fancy and parse out all of the browser string. Simply print out the information that is there. Display the percentage of all browser types that each line represents.'
 echo
 echo Percent Count Browser
-#n=($(curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | grep '^$' -v | wc -l))
-#curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | grep '^$' -v | sort | uniq -c | sort -nr | head -n 10 | awk -v n=$n '{print $1/n*100 "% " $0}'
+n=($(curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | grep '^$' -v | wc -l))
+curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | grep '^$' -v | sort | uniq -c | sort -nr | head -n 10 | awk -v n=$n '{print $1/n*100 "% " $0}'
 echo
 
 # 4. List the number of 404 errors that were reported in the log.
 echo '4. List the number of 404 errors that were reported in the log.'
 echo
 echo Count
-#curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 == 404) {print $9}}' | wc -l
+curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 == 404) {print $9}}' | wc -l
 echo
 
 # 5. List the number of 500 errors that were reported in the log.
 echo '5. List the number of 500 errors that were reported in the log.'
 echo
 echo Count
-#curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 == 500) {print $9}}' | wc -l
+curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{if ($9 == 500) {print $9}}' | wc -l
 echo
 
 # 6. What time of day is the site active? When is it quiet?
 echo '6. What time of day is the site active? When is it quiet?'
 echo
 echo Count Hour
-#curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ':' -f 2 | sort | uniq -c | sort -nr
+curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ':' -f 2 | sort | uniq -c | sort -nr
 echo
 echo 'The site is most active during the hour 15:00-16:00 (3-4 PM CST). There is high activity from 12:00-18:00 (12-6 PM CST).'
 echo 'The site is the most quiet during the hour 5:00-6:00 (5-6 AM CST). There is low activity from 3:00-8:00 (3-8 AM CST).'
@@ -54,4 +54,16 @@ echo
 
 # 7. Is the traffic "real" or mostly the result of robots or automated processes?
 echo '7. Is the traffic "real" or mostly the result of robots or automated processes?'
+echo
+# Look at some browser names:
+# curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | grep '^$' -v | grep Mozilla -v | sort | uniq -c | sort -nr | head -n 50
+n=($(curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | wc -l))
+# Real:
+r=($(curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | grep -iv -e spider -e bot -e crawler -e fetcher | wc -l))
+# Bots:
+b=($(curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | cut -d ' ' -f 12- | grep -i -e spider -e bot -e crawler -e fetcher | wc -l))
+echo The traffic is mostly real.
+echo $((100*$r/$n))% of the traffic is real.
+echo $((100*$b/$n))% of the traffic is the result of robots or automated processes.
+echo
 echo
