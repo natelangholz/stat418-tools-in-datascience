d86e9b3a0436d753fecc7715d53ffa4ab27b178f
Leon Zhu <leeeonzhu@gmail.com>
2019-04-23 17:01:31 -0700

    Update Hw1
    Update Hw1

--- week-2/hw1/homework-submissions/hw1_leon_zhu.sh
+++ week-2/hw1/homework-submissions/hw1_leon_zhu.sh
@@ -3,46 +3,16 @@
 # 1)
 echo "Question 1"
 curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '$9 != 404' | awk '{print $11}' | sort | uniq -c | sort -r | head -n 10
-# 28332 "-"
-# 10133 "http://users.csc.tntech.edu/~mjkosa/"
-# 3609
-# 3544 "http://users.csc.tntech.edu/~mjkosa/2011s07/CSC%202011%20Lab%20Syllabus.htm"
-# 2882 "http://users.csc.tntech.edu/~srini/DM/chapters/review4.3.html"
-# 2799 "http://users.csc.tntech.edu/~CSJamalpur21/"
-# 2728 "http://users.csc.tntech.edu/~srini/DM/chapters/review3.4.html"
-# 2556 "http://users.csc.tntech.edu/~srini/DM/chapters/review2.4.html"
-# 2482 "http://users.csc.tntech.edu/~sjcrook/westciv/default.css"
-# 1723 "http://users.csc.tntech.edu/~jley/"
 echo " "
 
 # 2)
 echo "Question 2"
 curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '$9 != 404' | awk '{print $7}' | sort | uniq -c | sort -r | head -n 10
-# 1724 /~mjkosa/
-# 1399 /~mjkosa/MarthaLeeds.jpg
-# 1395 /~mjkosa/computerscience.jpg
-# 1388 /~mjkosa/wvonline.jpg
-# 1387 /~mjkosa/magyar.gif
-# 1376 /~mjkosa/miniscruffy.jpg
-# 1376 /~mjkosa
-# 1350 /~mjkosa/2011s07/CSC%202011%20Lab%20Syllabus.htm
-# 1324 /~mjkosa/2011s07/CSC%202011%20Lab%20Syllabus_files/mystyle.css
-# 1308 /~mjkosa/miniwhitey.jpg
 echo " "
 
 # 3)
 echo "Question 3"
 curl -s http://users.csc.tntech.edu/~elbrown/access_log.bz2 | bunzip2 - | awk '{print $12}' | sort | uniq -c | sort -r | awk '{print $1/227574, $0}' |head -n 10
-# 52942 "Mozilla/5.0
-# 46291 "Mozilla/4.0
-# 5078 "sogou
-# 3609
-# 900 "msnbot-media/1.0
-# 775 "msnbot/1.0
-# 738 "Microsoft-WebDAV-MiniRedir/5.1.2600"
-# 555 "Feedfetcher-Google;
-# 512 "AppleSyndication/54"
-# 468 "Opera/9.10
 echo " "
 
 # 4)
